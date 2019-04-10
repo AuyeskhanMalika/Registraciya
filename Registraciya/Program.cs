9aab1dbd92ab7bde4ab630109269d4b035459de6
AuyeskhanM <auyeskhanm@gmail.com>
2019-04-10 23:32:12 +0600

    Добавьте файлы проекта.

---
+++ Registraciya/Program.cs
@@ -0,0 +1,19 @@
+﻿using Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registraciya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check registration = new Check();
+            registration.ValidationCheck();
+            registration.SendSms();
+        }
+    }
+}
